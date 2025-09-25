8426686d772a8fa9bbf0c0021d49d39e70d16aa2
chenzc <80132938@qq.com>
2025-09-25 10:02:22 +0800

    add

--- tests/test_env.rs
+++ tests/test_env.rs
@@ -6,10 +6,12 @@
 #![allow(unused_variables)]
 
 use std::{env, sync::{Arc}};
+use actix_web::rt::task;
 use log::info;
 use rust_inquire::{init, utils};
 use sqlx::{database, mysql::MySqlPool, Executor};
-use tokio::sync::Mutex;
+use tokio::{fs::{self, File}, io::{AsyncReadExt, BufReader}, stream, sync::{mpsc, Mutex}, task::JoinSet};
+use tokio_stream::{self, StreamExt};
 
 #[test]
 fn it_env_test() {
@@ -195,3 +197,72 @@ async fn it_get_all_test01() -> anyhow::Result<()> {
     
     Ok(())
 }
+
+#[tokio::test]
+async fn it_channel_test05()  {
+    init();
+    let bufer_size = 10000;
+    // 有界通道防爆内存
+    let (tx, mut rx) = mpsc::channel::<u32>(bufer_size);
+
+    // 生产者
+    let producer = tokio::spawn(async move {
+        for i in 0..bufer_size as u32 {
+            tx.send(i).await.unwrap();
+        }
+    });
+
+    // 消费者：buffer_unordered 控制并发度
+    // let mut tasks = tokio_stream::iter(0..bufer_size).map(|_| rx.recv());
+    // let mut buffered = tasks.buffer_unordered(100);
+    // while let Some(Some(num)) = buffered.next().await {
+    //     info!("Processed: {}", num);
+    // }
+
+    producer.await.unwrap();
+
+}
+
+#[tokio::test]
+async fn it_async_read_file_test01() -> anyhow::Result<()> {
+    init();
+    let file_path = "http-test.http";
+    let file = File::open(file_path).await?;
+    let mut reader = BufReader::new(file);
+    let mut contents = String::new();
+
+    // 异步读取 
+    reader.read_to_string(&mut contents).await?;
+
+    // 重 IO 处理; 用 spawn_blocking 
+    let processed = task::spawn_blocking(move || {
+        // 模拟 CPU/IO 混合, 如解析
+        contents.lines().count()
+    }).await?;
+
+    info!("lines: {}", processed);
+
+    Ok(())
+}
+
+// 复制多个大文件，IO 密集。
+#[tokio::test]
+async fn it_copy_test01() -> anyhow::Result<()> {
+    init();
+    let mut set = JoinSet::new();
+
+    for i in 0..10 {
+        set.spawn(async move {
+            let src = format!("src{}.bin", i);
+            let dst = format!("dst{}.bin", i);
+            fs::copy(&src, &dst).await
+        });
+    }
+
+    while let Some(res) = set.join_next().await {
+        res??;
+    }
+
+    Ok(())
+}
+
