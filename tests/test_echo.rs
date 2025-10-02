e0d1b0565e0b60a46c7893360e64abd74fbae8c2
chenzhichao <80139283@qq.com>
2025-10-02 21:55:00 +0800

    add

--- tests/test_echo.rs
+++ tests/test_echo.rs
@@ -7,7 +7,7 @@
 use dotenv::dotenv;
 use log::info;
 
-const ECHO_SERVER_ADDRESS: &str = "localhost:8887";
+const ECHO_SERVER_ADDRESS: &str = "localhost:8000";
 
 
 
@@ -16,8 +16,38 @@ mod tests {
     use std::{io::{Read, Write}, net::TcpStream};
 
     use rust_inquire::init;
+    use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
     use super::*;
+
+    #[tokio::test]
+    async fn it_echo_client_tokio_test() {
+        init();
+        // connection 
+        info!("connecting to {}", ECHO_SERVER_ADDRESS);
+
+        if let Ok(mut stream) = tokio::net::TcpStream::connect(ECHO_SERVER_ADDRESS).await {
+            // connected
+            info!("connected to echo server {}:{}",
+                stream.local_addr().unwrap().ip(),
+                stream.local_addr().unwrap().port()
+            );
+
+            // write a hello world message 
+            let message = "hellow rold";
+            let _ = stream.write_all(message.as_bytes()).await;
+            info!("send: {}", message);
+
+            // read the result
+            let mut buffer: [u8; 1024] = [0; 1024];
+            let len = stream.read(&mut buffer).await.unwrap();
+            let message = String::from_utf8_lossy(&buffer[..len]);
+            info!("received: {}", message);
+
+        } else {
+            info!("failed to connect to echo server {}", ECHO_SERVER_ADDRESS);
+        }
+    }
 
     #[tokio::test]
     async fn it_test01() {
